c65b629d02316496f304df13f035806b00602823
Conor Walsh <cw.conorwalsh@gmail.com>
2018-01-02 11:54:42 +0000

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/assets/img/favicon.png">
+
+    <title>Concur Digital Media</title>
+
+    <!-- CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top px-5">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="/assets/img/brand-white.png" alt="Concur" height="15px"></a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link scroll-link" href="#what-we-do">What We Do</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scroll-link" href="#why-us">Why Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scroll-link" href="#clients">Clients</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scroll-link" href="#careers">Careers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scroll-link" href="#contact">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div id="header">
+        <div id="header-cover">
+                <img id="logo" src="/assets/img/logo-white.png" alt="logo" class="img-fluid">
+        </div>
+    </div>
+
+
+    <div id="what-we-do" class="">
+        <h2>
+            What We Do
+        </h2>
+        <br>
+        <div class="row">
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-content.png" alt="icon"><br>
+                Content Management<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-dev.png" alt="icon"><br>
+                Web Development<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-list.png" alt="icon"><br>
+                SEO and PPC<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-mail.png" alt="icon"><br>
+                Newsletter Management<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-msg.png" alt="icon"><br>
+                Social Media Management<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-pin.png" alt="icon"><br>
+                Event Organisors<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-path.png" alt="icon"><br>
+                Brand Direction<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-computer.png" alt="icon"><br>
+                Web Management<br><br>
+            </div>
+        </div>
+    </div>
+
+    <div id="why-us" class="navy-bg">
+      <h2>
+            Why Us
+        </h2>
+        <div class="row">
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-responsive.png" alt="icon"><br>
+                Responsive Design<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-pen.png" alt="icon"><br>
+                Experienced<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-world.png" alt="icon"><br>
+                Worldwide<br><br>
+            </div>
+            <div class="col-sm-3">
+                <img id="logo" src="/assets/img/icons/icon-clock.png" alt="icon"><br>
+                Fast Development<br><br>
+            </div>
+        </div>
+    </div>
+
+    <div id="careers" class="parrollax">
+      <a href="#" class="btn btn-lg btn-primary">View Open Positions</a>
+    </div>
+
+
+    <div class="container marketing">
+      
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">The Future is Online. <span class="text-muted">Be a part of it.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 push-md-5">
+          <h2 class="featurette-heading">Increase Traffic. <span class="text-muted">Increase Sales.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5 pull-md-7">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Measure Success. <span class="text-muted">Analytics.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+    </div><!-- /.container -->
+
+    <div id="contact" class="white-bg">
+
+                <form class="form-horizontal" role="form" method="post" action="/send-pledge" id="pledgeform">
+                    {{ csrf_field() }}
+                    <h2>
+                      Contact Us
+                    </h2>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" id="plege-name" name="plege-name" placeholder="Name" value="" required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" id="plege-company" name="plege-company" placeholder="Company" value="" required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                        </div>
+                    </div>
+                </form>
+
+    </div>
+
+      <!-- FOOTER -->
+    <footer>
+      <p class="float-right"><a class="scroll-link" href="#header">Back to top</a></p>
+      <p>Made with love in the Sunny South East &copy; 2017 CONCUR Digital Media. &middot; All Rights Reserved <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
+
+    
+
+
+    <!-- JavaScript
+    ================================================== -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script src="/js/popper.min.js"></script>
+      <script src="/js/bootstrap.min.js"></script>
+      <script src="/js/scroll.js"></script>
+      <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
